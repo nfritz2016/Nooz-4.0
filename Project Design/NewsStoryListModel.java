ca941ce278cbcb44b292e09d27462bb504777f20
Nathan Fritz <nfritz2016@gmail.com>
2017-04-03 17:53:39 -0500

    Update NewsStoryListModel.java

--- Project Design/NewsStoryListModel.java
+++ Project Design/NewsStoryListModel.java
@@ -5,28 +5,27 @@ public class NewsStoryListModel {
 	
 	private ArrayList<ActionListener> ActionListenerList;
 	
-	public NewsStoryListModel()
-	{}
+	public NewsStoryListModel() {
+	}
 	
-	public void addNewsStory(NewsStory newsStory)
-	{}
+	public void addNewsStory(NewsStory newsStory) {
+	}
 	
-	public void editNewsStory(NewsStory newsStory)
-	{}
+	public void editNewsStory(NewsStory newsStory) {
+	}
 	
-	public void deleteNewsStory(NewsStory newsStory)
-	{}
+	public void deleteNewsStory(NewsStory newsStory) {
+	}
 	
-	public void deleteAllNewsStory(NewsStory newsStory)
-	{}
+	public void deleteAllNewsStory(NewsStory newsStory) {
+	}
 	
-	public synchronized void addActionListener(ActionListener l)
-	{}
+	public synchronized void addActionListener(ActionListener l) {
+	}
 	
-	public synchronized void removeActionListener(ActionListener l)
-	{}
+	public synchronized void removeActionListener(ActionListener l) {
+	}
 	
-	private void processEvent(ActionEvent e)
-	{}
-
+	private void processEvent(ActionEvent e) {
+	}
 }
