d465d61a21863596c239d8ced0903e6109ea3e42
Nathan Fritz <nfritz2016@gmail.com>
2017-04-03 22:58:12 -0500

    Add files via upload

---
+++ Project Design/NewsMakerListController.java
@@ -0,0 +1,47 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Controller for NewsMakerList.
+ * 
+ * @author Nathan Fritz, Joe Pauly, Cavan Gary
+ *
+ */
+public class NewsMakerListController {
+	/**
+	 * Model for the display.
+	 */
+	private NewsMakerListModel model;
+	
+	/**
+	 * Default Constructor.
+	 */
+	public NewsMakerListController() {
+		
+	}
+	
+	/**
+	 * Listes to the add newsstory button.
+	 * 
+	 * @author Nathan Fritz, Joe Pauly, Cavan Gary
+	 *
+	 */
+	public class AddNewsStoryListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	
+	/**
+	 * Listens to the clear NewsStoryButton.
+	 * @author Nathan Fritz, Joe Pauly, Cavan Gary
+	 *
+	 */
+	public class ClearNewsStoryListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+}
---
+++ Project Design/NewsStoryListController.java
@@ -0,0 +1,46 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Class that controls the display of the stories to the user.
+ * 
+ * @author Nathan Fritz, Joe Pauly, Cavan Gary
+ *
+ */
+public class NewsStoryListController {
+	/**
+	 * Model we use for the display.
+	 */
+	private NewsStoryListModel model;
+	
+	/**
+	 * Default constructor.
+	 */
+	public NewsStoryListController() {
+		
+	}
+	/**
+	 * Listes to the add newsstory button.
+	 * 
+	 * @author Nathan Fritz, Joe Pauly, Cavan Gary
+	 *
+	 */
+	public class AddNewsStoryListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+	
+	/**
+	 * Listens to the clear NewsStoryButton.
+	 * @author Nathan Fritz, Joe Pauly, Cavan Gary
+	 *
+	 */
+	public class ClearNewsStoryListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+		}
+	}
+}
