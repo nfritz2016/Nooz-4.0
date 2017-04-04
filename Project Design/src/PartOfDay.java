c943b3aa4466b369ca24c1506fbd7d4a20fcc36a
josephpauly <joseph.b.pauly-1@ou.edu>
2017-04-03 23:08:09 -0500

    Update PartOfDay.java

--- Project Design/src/PartOfDay.java
+++ Project Design/src/PartOfDay.java
@@ -1,10 +1,25 @@
 
+/**
+ * Enumeration for the part of day that
+ * a story may be captured, published, or printed.
+ *
+ * @author Nathan Fritz - 113294199
+ * @author Joe Pauly - 112909034
+ * @author Cavan Gary - 113388151
+ *
+ */
 public enum PartOfDay {
 	MORNING, 
 	AFTERNOON, 
 	EVENING, 
 	LATE_NIGHT;
 
+	/**
+	 * Overridden toString method will convert
+	 * the enum into a usable String.
+	 * 
+	 * @return the String version of the part of day.
+	 */
 	@Override
 	public String toString() {
 		return null; //TODO revert return
