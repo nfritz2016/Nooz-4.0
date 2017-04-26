a501cbde9a0a9ca053e6a62d43d5f0922e575224
josephpauly <joseph.b.pauly-1@ou.edu>
2017-04-25 20:41:49 -0500

    Just StubCode. Working on methods.
    Waiting on Dean to give us code for Listener(s)

---
+++ src/NewsController.java
@@ -0,0 +1,165 @@
+import java.util.List;
+import javax.swing.JDialog;
+
+public class NewsController {
+
+	private NewsDataBaseModel newsDataBaseModel;
+	private SelectionView selectionView;
+	private EditNewsMakerView editNewsMakerView;
+	private JDialog viewDialog;
+	private AddEditNewsStoryView addEditNewsStoryView;
+	private NewsStory editedNewsStory;
+	private MediaTypeSelectionView mediaTypeSelectionView;
+	private List<NewsMedia> selectedMediaTypes;
+	
+	
+	//TODO write constructor
+	public NewsController(){
+		
+	}
+	
+	//TODO write 
+	public void setNewsDataBaseModel(NewsDataBaseModel){
+		
+	}
+	
+	//TODO write
+	public void setSelectionView(SelectionView){
+		
+	}
+	
+	//TODO write
+	private void loadNewsData(){
+		
+	}
+	
+	//TODO write
+	private void saveNewsData(){
+		
+	}
+	
+	//TODO write
+	private void importNoozStories(){
+		
+	}
+	
+	//TODO write
+	private void exportNewsStories(){
+		
+	}
+	
+	//TODO write
+	private void addNewsMaker(){
+		
+	}
+	
+	//TODO write
+	private void editNewsMakers(){
+		
+	}
+	
+	//TODO write
+	private void deleteNewsMakers(){
+		
+	}
+	
+	//TODO write
+	private void deleteNewsMakerList(){
+		
+	}
+	
+	//TODO write
+	private void addNewsStory(){
+		
+	}
+	
+	//TODO write
+	private void editNewsStories(){
+		
+	}
+	
+	//TODO write
+	private void sortNewsStories(){
+		
+	}
+	
+	//TODO write
+	private void deleteNewsStories(){
+		
+	}
+	
+	//TODO write
+	private void deleteAllNewsStories(){
+		
+	}
+	
+	//TODO write
+	private void displayPieCharts(){
+		
+	}
+	
+	//TODO write
+	private void displayTextViews(){
+		
+	}
+	
+	
+	//LISTENERS
+	//TODO write listeners
+	
+	private class FileMenuListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class NewsMakerMenuListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class NewsStoryMenuListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class DisplayMenuListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class EditNewsMakerNameListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class RemoveNewsMakerFromNewStoriesListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class AddEditNewsStoryListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+	
+	private class MediaTypeSelectionListener{
+		//TODO write
+		public void actionPerformed(ActionEvent actionEvent){
+			
+		}
+	}
+}
