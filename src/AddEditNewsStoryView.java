ea70746fac7ab2212eb128b1a5fb793ac2e58817
Nathan Fritz <nfritz2016@gmail.com>
2017-04-25 20:36:41 -0500

    Fully complete(except other class implementations)

--- src/AddEditNewsStoryView.java
+++ src/AddEditNewsStoryView.java
@@ -1,15 +1,11 @@
-import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.io.Serializable;
 import java.text.NumberFormat;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class AddEditNewsStoryView extends JPanel implements Serializable {
 
@@ -19,50 +15,50 @@ public class AddEditNewsStoryView extends JPanel implements Serializable {
 	//contents for type
 	private JLabel jlbNewsMediaType = new JLabel("Type: ");
 	JComboBox<NewsMedia> jcbNewsMediaType = new JComboBox<NewsMedia>();
-	private JPanel jpNewsMediaType = new JPanel(new GridLayout(1, 2));
+	private JPanel jpNewsMediaType = new JPanel();
 	//contents for source
 	private JLabel jlbNewsStorySource = new JLabel("Source: ");
 	JComboBox<String> jcbNewsStorySource = new JComboBox<String>();
-	private JPanel jpNewsStorySource = new JPanel(new GridLayout(1, 2));
+	private JPanel jpNewsStorySource = new JPanel();
 	//contents for topic
 	private JLabel jlbNewsStoryTopic = new JLabel("Topic: ");
 	JComboBox<String> jcbNewsStoryTopic = new JComboBox<String>();
-	private JPanel jpNewsStoryTopic = new JPanel(new GridLayout(1, 2));
+	private JPanel jpNewsStoryTopic = new JPanel();
 	//contents of subject
 	private JLabel jlbNewsStorySubject = new JLabel("Subject: ");
 	JComboBox<String> jcbNewsStorySubject = new JComboBox<String>();
-	private JPanel jpNewsStorySubject = new JPanel(new GridLayout(1, 2));
+	private JPanel jpNewsStorySubject = new JPanel();
 	//contents of NewsMaker1
 	private JLabel jlbNewsStoryNewsMaker1 = new JLabel("News Maker 1: ");
 	JComboBox<String> jcbNewsStoryNewsMaker1 = new JComboBox<String>();
-	private JPanel jpNewsStoryNewsMaker1 = new JPanel(new GridLayout(1, 2));
+	private JPanel jpNewsStoryNewsMaker1 = new JPanel();
 	//contents of NewsMaker2
 	private JLabel jlbNewsStoryNewsMaker2 = new JLabel("News Maker 2: ");
 	JComboBox<String> jcbNewsStoryNewsMaker2 = new JComboBox<String>();
-	private JPanel jpNewsStoryNewsMaker2 = new JPanel(new GridLayout(1, 2));
+	private JPanel jpNewsStoryNewsMaker2 = new JPanel();
 	//contents of length field
 	private JLabel jlbNewsStoryLength = new JLabel("Length: ");
 	private NumberFormat integerFieldFormatter = NumberFormat.getNumberInstance();
 	JFormattedTextField jftfNewsStoryLength = new JFormattedTextField(integerFieldFormatter);
-	private JPanel jplNewsStoryLength = new JPanel(new GridLayout(1, 2));
+	private JPanel jplNewsStoryLength = new JPanel();
 	//contents of year field
 	private JLabel jlbNewsStoryYear = new JLabel("Year: ");
 	private Integer[] years;
 	JComboBox<Integer> jcbNewsStoryYear = new JComboBox<Integer>();
-	private JPanel jplNewsStoryYear = new JPanel(new GridLayout(1, 2));
+	private JPanel jplNewsStoryYear = new JPanel();
 	//contents of month field
 	private JLabel jlbNewsStoryMonth = new JLabel("Month: ");
 	JComboBox<Month> jcbNewsStoryMonth = new JComboBox<Month>();
-	private JPanel jplNewsStoryMonth = new JPanel(new GridLayout(1, 2));
+	private JPanel jplNewsStoryMonth = new JPanel();
 	//contents of day
 	private JLabel jlbNewsStoryDay = new JLabel("Day: ");
 	private Integer[] days;
 	JComboBox<Integer> jcbNewsStoryDay = new JComboBox<Integer>();
-	private JPanel jplNewsStoryDay = new JPanel(new GridLayout(1, 2));
+	private JPanel jplNewsStoryDay = new JPanel();
 	//contents of PartOfDay field
 	private JLabel jlbNewsStoryPartOfDay = new JLabel("Part of Day: ");
 	JComboBox<PartOfDay> jcbNewsStoryPartOfDay = new JComboBox<PartOfDay>();
-	private JPanel jplNewsStoryPartOfDay = new JPanel(new GridLayout(1, 2));
+	private JPanel jplNewsStoryPartOfDay = new JPanel();
 	//panel for when details
 	private JPanel jplNewsStoryWhen = new JPanel(new GridLayout(1, 4));
 	//button to do everything
@@ -109,20 +105,16 @@ public class AddEditNewsStoryView extends JPanel implements Serializable {
 		jplNewsStoryWhen.add(jplNewsStoryDay);
 		jplNewsStoryWhen.add(jplNewsStoryPartOfDay);
 		
-		jplAddEditNewsStory.add(jpNewsMediaType, JPanel.CENTER_ALIGNMENT);
-		jplAddEditNewsStory.add(jpNewsStorySource);
-		jplAddEditNewsStory.add(jpNewsStoryTopic);
-		jplAddEditNewsStory.add(jpNewsStorySubject);
-		jplAddEditNewsStory.add(jpNewsStoryNewsMaker1);
-		jplAddEditNewsStory.add(jpNewsStoryNewsMaker2);
-		jplAddEditNewsStory.add(jplNewsStoryLength);
-		jplAddEditNewsStory.add(jplNewsStoryWhen);
 		jplAddEditNewsStory.add(jbtAddEditNewsStory);
 		
-		JFrame frame = new JFrame("Test");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(jplAddEditNewsStory);
-		frame.pack();
-		frame.setVisible(true);
-;	}
+		add(jpNewsMediaType);
+		add(jpNewsStorySource);
+		add(jpNewsStoryTopic);
+		add(jpNewsStorySubject);
+		add(jpNewsStoryNewsMaker1);
+		add(jpNewsStoryNewsMaker2);
+		add(jplNewsStoryLength);
+		add(jplNewsStoryWhen);
+		add(jplAddEditNewsStory);
+	}
 }
