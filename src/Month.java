9e5cb00d92a7bd3a82aea560fb758dc2f631b433
cgary12 <cgary1213@yahoo.com>
2017-04-25 20:35:02 -0500

    Done/Just check one method param

--- src/Month.java
+++ src/Month.java
@@ -4,7 +4,8 @@
  *
  */
 public enum Month {
-	JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+	JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, 
+	JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
 	
 	@Override
 	public String toString() {
@@ -36,14 +37,76 @@ public enum Month {
 		default:
 			throw new IllegalArgumentException();
 		}
+	}
 		
-		//TODO: Figure out what this method is in UML
-		public int fromInt() {
+	public int toInt(){
+			
+			switch(this){
+			case JANUARY:
+				return 1;
+			case FEBRUARY:
+				return 2;
+			case MARCH:
+				return 3;
+			case APRIL:
+				return 4;
+			case MAY:
+				return 5;
+			case JUNE:
+				return 6;
+			case JULY:
+				return 7;
+			case AUGUST:
+				return 8;
+			case SEPTEMBER:
+				return 9;
+			case OCTOBER:
+				return 10;
+			case NOVEMBER:
+				return 11;
+			case DECEMBER:
+				return 12;
+			default:
+				throw new IllegalArgumentException();
 			
 		}
-		
-		
-		
+			
 	}
-
+	
+	
+	//TODO: Check to see if num is good parameter for mimir
+	public static Month fromInt(int num){
+		switch(num){
+		case 1:
+			return JANUARY;
+		case 2:
+			return FEBRUARY;
+		case 3:
+			return MARCH;
+		case 4:
+			return APRIL;
+		case 5:
+			return MAY;
+		case 6:
+			return JUNE;
+		case 7:
+			return JULY;
+		case 8:
+			return AUGUST;
+		case 9:
+			return SEPTEMBER;
+		case 10:
+			return OCTOBER;
+		case 11:
+			return NOVEMBER;
+		case 12:
+			return DECEMBER;
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+	
+			
 }
+
+
