7866185b7df657983d93a79e241bb7b93edda7bf
Nathan Fritz <nfritz2016@gmail.com>
2017-04-30 19:12:39 -0500

    Believe this to be finished

--- src/AddEditNewsStoryView.java
+++ src/AddEditNewsStoryView.java
@@ -7,34 +7,38 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * @author Nathan Fritz
+ *
+ */
 public class AddEditNewsStoryView extends JPanel implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private NewsDataBaseModel newsDataBaseModel = new NewsDataBaseModel();
-	private NewsStory newsStory = new NewsStory();
+	private NewsStory newsStory;
 	//contents for type
 	private JLabel jlbNewsMediaType = new JLabel("Type: ");
-	JComboBox<NewsMedia> jcbNewsMediaType = new JComboBox<NewsMedia>();
+	JComboBox<NewsMedia> jcbNewsMediaType = new JComboBox<NewsMedia>(NewsMedia.values());
 	private JPanel jpNewsMediaType = new JPanel();
 	//contents for source
 	private JLabel jlbNewsStorySource = new JLabel("Source: ");
-	JComboBox<String> jcbNewsStorySource = new JComboBox<String>();
+	JComboBox<String> jcbNewsStorySource = new JComboBox<String>(newsDataBaseModel.getNewsSources());
 	private JPanel jpNewsStorySource = new JPanel();
 	//contents for topic
 	private JLabel jlbNewsStoryTopic = new JLabel("Topic: ");
-	JComboBox<String> jcbNewsStoryTopic = new JComboBox<String>();
+	JComboBox<String> jcbNewsStoryTopic = new JComboBox<String>(newsDataBaseModel.getNewsTopics());
 	private JPanel jpNewsStoryTopic = new JPanel();
 	//contents of subject
 	private JLabel jlbNewsStorySubject = new JLabel("Subject: ");
-	JComboBox<String> jcbNewsStorySubject = new JComboBox<String>();
+	JComboBox<String> jcbNewsStorySubject = new JComboBox<String>(newsDataBaseModel.getNewsSubjects());
 	private JPanel jpNewsStorySubject = new JPanel();
 	//contents of NewsMaker1
 	private JLabel jlbNewsStoryNewsMaker1 = new JLabel("News Maker 1: ");
-	JComboBox<String> jcbNewsStoryNewsMaker1 = new JComboBox<String>();
+	JComboBox<String> jcbNewsStoryNewsMaker1 = new JComboBox<String>(newsDataBaseModel.getNewsMakerNames());
 	private JPanel jpNewsStoryNewsMaker1 = new JPanel();
 	//contents of NewsMaker2
 	private JLabel jlbNewsStoryNewsMaker2 = new JLabel("News Maker 2: ");
-	JComboBox<String> jcbNewsStoryNewsMaker2 = new JComboBox<String>();
+	JComboBox<String> jcbNewsStoryNewsMaker2 = new JComboBox<String>(newsDataBaseModel.getNewsMakerNames());
 	private JPanel jpNewsStoryNewsMaker2 = new JPanel();
 	//contents of length field
 	private JLabel jlbNewsStoryLength = new JLabel("Length: ");
@@ -43,30 +47,33 @@ public class AddEditNewsStoryView extends JPanel implements Serializable {
 	private JPanel jplNewsStoryLength = new JPanel();
 	//contents of year field
 	private JLabel jlbNewsStoryYear = new JLabel("Year: ");
-	private Integer[] years;
-	JComboBox<Integer> jcbNewsStoryYear = new JComboBox<Integer>();
+	private Integer[] years = {2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017};
+	JComboBox<Integer> jcbNewsStoryYear = new JComboBox<Integer>(years);
 	private JPanel jplNewsStoryYear = new JPanel();
 	//contents of month field
 	private JLabel jlbNewsStoryMonth = new JLabel("Month: ");
-	JComboBox<Month> jcbNewsStoryMonth = new JComboBox<Month>();
+	JComboBox<Month> jcbNewsStoryMonth = new JComboBox<Month>(Month.values());
 	private JPanel jplNewsStoryMonth = new JPanel();
 	//contents of day
 	private JLabel jlbNewsStoryDay = new JLabel("Day: ");
-	private Integer[] days;
-	JComboBox<Integer> jcbNewsStoryDay = new JComboBox<Integer>();
+	private Integer[] days = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+	JComboBox<Integer> jcbNewsStoryDay = new JComboBox<Integer>(days);
 	private JPanel jplNewsStoryDay = new JPanel();
 	//contents of PartOfDay field
 	private JLabel jlbNewsStoryPartOfDay = new JLabel("Part of Day: ");
-	JComboBox<PartOfDay> jcbNewsStoryPartOfDay = new JComboBox<PartOfDay>();
+	JComboBox<PartOfDay> jcbNewsStoryPartOfDay = new JComboBox<PartOfDay>(PartOfDay.values());
 	private JPanel jplNewsStoryPartOfDay = new JPanel();
 	//panel for when details
 	private JPanel jplNewsStoryWhen = new JPanel(new GridLayout(1, 4));
 	//button to do everything
-	JButton jbtAddEditNewsStory = new JButton("Test");
-	//panel to contain EVERYTHING
+	JButton jbtAddEditNewsStory;
+	//panel to contain button
 	private JPanel jplAddEditNewsStory = new JPanel(new GridLayout(9, 1, 12, 12));
 	
-	public AddEditNewsStoryView(/*NewsDataBaseModel newsDataBaseModel, NewsStory newsStory*/) {
+	public AddEditNewsStoryView(NewsDataBaseModel newsDataBaseModel, NewsStory newsStory) {
+		this.newsDataBaseModel = newsDataBaseModel;
+		this.newsStory = newsStory;
+		
 		jpNewsMediaType.add(jlbNewsMediaType);
 		jpNewsMediaType.add(jcbNewsMediaType);
 		
@@ -116,5 +123,32 @@ public class AddEditNewsStoryView extends JPanel implements Serializable {
 		add(jplNewsStoryLength);
 		add(jplNewsStoryWhen);
 		add(jplAddEditNewsStory);
+		
+		if (newsStory == null) {
+			jbtAddEditNewsStory.setText("Add News Story");
+		}
+		else {
+			jbtAddEditNewsStory.setText("Edit News Story");
+			
+			if (newsStory instanceof NewspaperStory) {
+				jcbNewsMediaType.setSelectedItem(NewsMedia.NEWSPAPER);
+			}
+			else if (newsStory instanceof TVNewsStory) {
+				jcbNewsMediaType.setSelectedItem(NewsMedia.TV);
+			}
+			else if (newsStory instanceof OnlineNewsStory) {
+				jcbNewsMediaType.setSelectedItem(NewsMedia.ONLINE);
+			}
+			
+			jcbNewsStorySource.setSelectedItem(newsStory.getSource());
+			jcbNewsStoryTopic.setSelectedItem(newsStory.getTopic());
+			jcbNewsStorySubject.setSelectedItem(newsStory.getSubject());
+			jcbNewsStoryNewsMaker1.setSelectedItem(newsStory.getNewsMaker1());
+			jcbNewsStoryNewsMaker2.setSelectedItem(newsStory.getNewsMaker2());
+			jcbNewsStoryYear.setSelectedItem(newsStory.getDate().getYear());
+			jcbNewsStoryMonth.setSelectedItem(newsStory.getDate().getMonth());
+			jcbNewsStoryDay.setSelectedItem(newsStory.getDate().getDayOfMonth());
+			jcbNewsStoryPartOfDay.setSelectedItem(newsStory.getDate());
+		}
 	}
 }
