13d6664977dbb275acf2689d7cd1e93b61666e04
Nathan Fritz <nfritz2016@gmail.com>
2017-04-30 19:45:12 -0500

    I think it is finished but not sure if done correctly

--- src/EditNewsMakerView.java
+++ src/EditNewsMakerView.java
@@ -1,6 +1,8 @@
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Serializable;
+import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -10,42 +12,50 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public class EditNewsMakerView implements Serializable, ActionListener {
-
-	/**
-	 * 
-	 */
+/**
+ * @author Nathan Fritz
+ *
+ */
+public class EditNewsMakerView extends JPanel implements ActionListener, Serializable {
 	private static final long serialVersionUID = 1L;
+	private NewsDataBaseModel newsDataBaseModel = new NewsDataBaseModel();
+	private DefaultListModel<String> newsStoryStringList = new DefaultListModel<String>();
+	private JList<String> jlNewsStoryList = new JList<String>();
+	private JScrollPane jspNewsStoryList = new JScrollPane(jlNewsStoryList);
+	private JPanel jpNewsStoryList = new JPanel();
+	JTextField jtfName = new JTextField();
+	private JLabel jlbName = new JLabel("Name: ");
+	JButton jbtRemoveFromStory = new JButton("Remove From Story");
+	private JPanel jplName = new JPanel();
 	
-	private NewsMakerModel newsMakerModel;
-	private NewsDataBaseModel newsDataBaseModel;
-	private DefaultListModel<String> newsStoryStringList;
-	private JList<String> jlNewsStoryList;
-	private JScrollPane jspNewsStoryList;
-	private JPanel jpNewsStoryList;
-	private JTextField jftName;
-	private JLabel jlbName;
-	private JButton jbtRemoveFromStory;
-	private JPanel jplName;
-	
-	//TODO write constructor
 	public EditNewsMakerView(NewsMakerModel newsMakerModel, NewsDataBaseModel newsDataBaseModel) {
-		
+		this.newsDataBaseModel = newsDataBaseModel;
+		jplName.add(jlbName);
+		jplName.add(jtfName);
+		jpNewsStoryList.add(jspNewsStoryList);
+		setLayout(new BorderLayout(1, 2));
+		add(jplName);
+		add(jpNewsStoryList);
 	}
 	
-	//TODO write method
 	public int[] getSelectedNewsStoryIndices() {
-		
+		return jlNewsStoryList.getSelectedIndices();
 	}
 	
-	//TODO write method
-	private void populateNewsStroyIndices() {
+	private void populateNewsStoryJList() {
+		DefaultListModel<NewsStory> duplicate = newsDataBaseModel.getNewsStories();
+		if (duplicate.isEmpty()) {
+			newsStoryStringList.clear();
+		}
 		
+		for (int i = 0; i < duplicate.getSize(); i++) {
+			newsStoryStringList.addElement(duplicate.get(i).toString());
+		}
 	}
 	
-	//TODO write method
 	public void actionPerformed(ActionEvent actionEvent) {
-		
+		if (actionEvent.getActionCommand().equals("Edit NewsMaker View")) {
+			populateNewsStoryJList();
+		}
 	}
-	
 }
