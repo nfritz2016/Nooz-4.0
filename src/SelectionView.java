5bf39a9d53c949eca14409a205ec655bbd8e398c
Nathan Fritz <nfritz2016@gmail.com>
2017-05-03 07:07:03 -0500

    i think im ded

--- src/SelectionView.java
+++ src/SelectionView.java
@@ -221,13 +221,16 @@ public class SelectionView extends JFrame implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent actionEvent) {
-		NewsMakerModel[] newsMakerModelArray = new NewsMakerModel[newsDataBaseModel.getNewsMakers().size()];
-		for (int i = 0; i < newsDataBaseModel.getNewsMakers().size(); i++) {
-			newsMakerModelArray[i] = newsDataBaseModel.getNewsMakerListModel().get(i);
+		if ("Add News Story".equals(actionEvent.getActionCommand())) {
+			NewsMakerModel[] newsMakerModelArray = new NewsMakerModel[newsDataBaseModel.getNewsMakers().size()];
+			for (int i = 0; i < newsDataBaseModel.getNewsMakers().size(); i++) {
+				newsMakerModelArray[i] = newsDataBaseModel.getNewsMakerListModel().get(i);
+			}
+			jlNewsMakerList.setListData(newsMakerModelArray);
+			
+			newsMakerModelArray[1] = new NewsMakerModel("none");
+			System.out.println("Actionperformed sview");
 		}
-		jlNewsMakerList.setListData(newsMakerModelArray);
-		
-		newsMakerModelArray[1] = new NewsMakerModel("none");
 		/*
 		if(actionEvent.getActionCommand().equals("Present News Stories")) {
 			if (getSelectedNewsMakers().length == 0) {
