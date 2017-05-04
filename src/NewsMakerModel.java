042e2cdd9df43c86f25c753f89ef87f16916822a
Nathan Fritz <nfritz2016@gmail.com>
2017-05-03 21:53:47 -0500

    tinker

--- src/NewsMakerModel.java
+++ src/NewsMakerModel.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
  * @author Nathan Fritz
  *
  */
-public class NewsMakerModel implements ActionListener, Serializable {
+public class NewsMakerModel implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -102,10 +102,5 @@ public class NewsMakerModel implements ActionListener, Serializable {
 			ActionListener listener = list.get(i);
 			listener.actionPerformed(e);
 		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		//TODO idk what to do here
 	}
 }
